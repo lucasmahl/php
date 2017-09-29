90191cac9aec7087bbbb3db9bba2d493b37b43f5
Lucas Mahl <lucasmahl@gmail.com>
2017-09-29 14:46:47 -0300

    Adicionado novos arquivos em /funções

--- funcoes/exemplo-03.php
+++ funcoes/exemplo-03.php
@@ -1,11 +1,12 @@
 <?php
 
-	function ola($texto = "mundo", $periodo = "Bom dia!" ){
+	function ola($texto /*= "mundo"*/, $periodo = "Bom dia!" ){//parametros obrigatórios devem sempre vir primeiro, como $texto
 
 		echo "Olar $texto! $periodo <br/>";
 	}
 
-	echo ola();
+	/*echo ola();*/
+	echo ola("Mundão");
 	echo ola("");
 	echo ola("Lucas","Boa tarde");
 	echo ola("Mundo","");
---
+++ funcoes/exemplo-05.php
@@ -0,0 +1,27 @@
+<?php
+
+	$a = 10;
+
+	function trocaValor($a){
+		$a += 50;
+		return $a;
+	}
+
+	echo $a;
+	echo "<br/>";
+	echo trocaValor($a);
+	echo "<br/>";
+	echo $a; //o trocaValor não alterou a variável
+
+//& = passagem de parametro por referência
+	function alteraValor(&$b){
+		$b += 50;
+		return $b;
+	}
+
+	echo "<br/>";
+	echo alteraValor($a);
+	echo "<br/>";
+	echo $a; 
+
+?>
