24160661b6f95708dcc60cc76af12ba3817f3442
Lucas Mahl <lucasmahl@gmail.com>
2017-10-03 16:01:56 -0300

    Ultimos arquivos do dia

---
+++ date/exemplo-01.php
@@ -0,0 +1,18 @@
+<?php
+
+	echo date("H:i:s - D/M - d/m/Y");
+
+	echo "<br/>";
+	echo time();
+
+
+	//testando data e hora local
+	/*date_default_timezone_set('America/Sao_Paulo');
+	$data = date('d/m/Y');
+	$hora = date('H:i:s');
+
+	echo "<br/>";
+	echo $data."<br/>";
+	echo $hora."<br/>";*/
+
+?>
---
+++ funcoes/funcao-anonima.php
@@ -0,0 +1,11 @@
+<?php
+
+	function test($callback){
+		//processo lento
+		$callback();
+	}
+
+	test(function(){
+		echo "Terminou!";
+	});
+?>
